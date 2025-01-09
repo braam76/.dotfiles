6275694f338d8623841b24f6722c4c6c799b00a4
Ilgar Gamidov <151990428+braam76@users.noreply.github.com>
2025-01-09 13:37:03 +0300

    Update init.el

--- init.el
+++ init.el
@@ -1,15 +1,3 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'noirblaze t)
-
-(require `package)
-(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 (tool-bar-mode             0)  ;; Remove toolbar
 (scroll-bar-mode           0)  ;; Remove scrollbars
 (menu-bar-mode             0)  ;; Remove menu bar
@@ -17,21 +5,32 @@
 (column-number-mode        t)  ;; Show column numbers
 (global-auto-revert-mode   t)  ;; Automatically update buffers
 
-
-(setq inhibit-splash-screen     t)  ;; Disable splash screen
-
-
 ;; creates file where customization variables will be loaded
 (setq custom-file (locate-user-emacs-file "custom-variables.el"))
 (load custom-file 'noerror 'nomessage)
 
-;; use packages
-(use-package zig-mode :ensure t)
-(use-package vterm    :ensure t)
+;; Unbind C-d for multiple-cursors
+(global-unset-key (kbd "C-d"))
 
-(use-package company-mode
-  :init
-  (global-company-mode))
+;; Load Elpaca setup
+(load-file (expand-file-name "elpaca-setup.el" user-emacs-directory))
 
-(use-package emacs
-  :hook (zig-mode . eglot-ensure))
+;; Enable use-package support for Elpaca
+(elpaca elpaca-use-package
+  (elpaca-use-package-mode))
+
+;; Install and configure multiple-cursors via use-package
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-d" . mc/mark-next-like-this)
+         ("C-<f2>" . mc/mark-all-like-this)
+	 ;; ("C-S-c C-S-c" . mc/edit-lines
+	 ;; ("C-<" . mc/mark-previous-like-this)
+	 ))
+(use-package)
+
+;; Load and apply the Noirblaze theme
+(use-package noirblaze-theme
+  :load-path "~/.emacs.d/themes/"
+  :config
+  (load-theme 'noirblaze t))
